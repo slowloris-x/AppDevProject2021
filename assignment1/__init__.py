53a2b8b13f9715dd6ba9e0bf02f341d7391e4f4a
tabbytan <weiheng203@gmail.com>
2021-12-23 15:53:18 +0800

    made base.html
    added navbar and footer to the base.html and made the home.html for testing

---
+++ assignment1/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+# from user_controller import user_controller
+
+app = Flask(__name__)
+# app.register_blueprint(user_controller)
+app.secret_key = 'MyFlaskWebAppKey'
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/contactUs')
+def contact_us():
+    return render_template('contactUs.html')
+
+
+if __name__ == '__main__':
+    app.run()
